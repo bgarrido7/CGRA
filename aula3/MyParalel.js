4ef867883eca75f327d4b4e3301fad1a69521e58
JoaoFula <JoaoFula@users.noreply.github.com>
2019-03-08 19:07:42 +0000

    ficamos a meio do ponto 3

---
+++ aula3/MyParalel.js
@@ -0,0 +1,35 @@
+class MyParalel extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			0, 1, 0,	//0
+			1, 0, 0,	//1
+			1, 1, 0,	//2
+			
+			2, 0, 0,	//3
+			2, 1, 0,	//4
+			3, 0, 0,	//5
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 1, 2,
+			2, 3, 4,
+			1, 3, 2,
+			3, 5, 4,
+		];
+
+		
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.normals = [];
+        for (var i = 0; i <= 5; i++) {
+            this.normals.push(0, 0, 1);
+		}
+		
+		this.initGLBuffers();
+	}
+}
---
+++ aula3/MyTriangle.js
@@ -0,0 +1,26 @@
+class MyTriangle extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			-1, -1, 0,	//0
+			-1, 1, 0,	//1
+			1, -1, 0,	//2
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 2, 1,
+		];
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.normals = [];
+        for (var i = 0; i <= 2; i++) {
+            this.normals.push(0, 0, 1);
+		}
+
+		this.initGLBuffers();
+	}
+}
