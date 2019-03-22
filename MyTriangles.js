fc47ecadad8aee9b10fd905f78280671877e6c3c
JoaoFula <JoaoFula@users.noreply.github.com>
2019-03-22 19:09:43 +0000

    Add files via upload

---
+++ MyTriangles.js
@@ -0,0 +1,30 @@
+class MyTriangles extends CGFobject {
+	constructor(scene) {
+		super(scene);
+		this.initBuffers();
+	}
+	initBuffers() {
+		this.vertices = [
+			0, 0, 0,	//0
+			-2, 0, 0,	//1
+			0, 2, 0,	//2
+			2, 2, 0,    //3
+		];
+
+		//Counter-clockwise reference of vertices
+		this.indices = [
+			0, 2, 1,   //1
+			0, 3, 2,   //2
+			
+			
+		];
+		this.primitiveType = this.scene.gl.TRIANGLES;
+
+		this.normals = [];
+        for (var i = 0; i <= 3; i++) {
+            this.normals.push(0, 0, 1);
+		}
+
+		this.initGLBuffers();
+	}
+}
