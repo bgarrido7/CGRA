9dc00b6a17d701ebe66be465888dcad93bc8e24e
bgarrido7 <bia@DESKTOP-SIQEO2L.localdomain>
2019-04-27 13:33:24 +0000

    tp6 começada

--- aula6/MyInterface.js
+++ aula6/MyInterface.js
@@ -15,11 +15,10 @@ class MyInterface extends CGFinterface {
         this.gui = new dat.GUI();
         
         var obj = this;
-
         
         this.gui.add(this.scene, 'axiom').name('Axiom');
-        this.gui.add(this.scene, "ruleX").name('Rule for X');
         this.gui.add(this.scene, "ruleF").name('Rule for F');
+        this.gui.add(this.scene, "ruleX").name('Rule for X');
 
         this.gui.add(this.scene, 'angle').name('Angle');
         this.gui.add(this.scene, 'iterations').min(0).step(1).name('Iterations');
